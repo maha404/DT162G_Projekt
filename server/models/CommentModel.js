17a8bfdc62a206640452fedd8ea0a28629d8cf2e
maha404 <117388545+maha404@users.noreply.github.com>
2023-11-27 16:21:35 +0100

    Skapade ett schema och modul för kommentarer till databasen.

---
+++ server/models/CommentModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    content: {
+        type: String, 
+        required: true
+    }, 
+    author: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "users",
+        required: true
+    }, 
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts", 
+        required: true
+    }
+}, { timestamps: true } )
+
+module.exports = mongoose.model('Comment', commentSchema);
