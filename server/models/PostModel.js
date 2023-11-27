5fc6ad17edb1ce3d38db327df45027483cc6949b
maha404 <117388545+maha404@users.noreply.github.com>
2023-11-27 16:22:04 +0100

    Skapade ett schema och modul för poster

--- server/models/PostModel.js
+++ server/models/PostModel.js
@@ -4,21 +4,21 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: {
         type: String, 
-        required: true
+        required: [true, 'En titlel måste finnas!']
     }, 
     content: {
         type: String, 
-        required: true
+        required: [true, 'Text måste finnas!']
     }, 
     author: {
         type: mongoose.Schema.Types.ObjectId, 
         ref:"users", 
-        required:true
+        required: true
     }, 
     comments: [{
         type: mongoose.Schema.Types.ObjectId, 
         ref: "comments"
     }]
-}, { timestamps: true })
+}, { timestamps: true }) // Lägger till datum för posten (Tillagd och uppdaterad)
 
 module.exports = mongoose.model('Post', postSchema)
