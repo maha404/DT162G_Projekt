d86b78dde786638a32839892aa478a4402e31be6
maha404 <117388545+maha404@users.noreply.github.com>
2023-11-27 15:27:52 +0100

    Lade till funktion för att uppdatera specifik post.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -40,8 +40,16 @@ router.get('/posts/:id', async (req, res) => {
     }
 })
 
-router.put('/posts/:id', (req, res) => {
-    res.json({msg: 'Specifik post uppdaterad!'})
+// Update specific post in database
+router.put('/posts/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const update = await Post.findByIdAndUpdate(id, req.body)
+        res.status(200).json(update)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+
 })
 
 router.get('/posts/mostrecent', (req, res) => {
