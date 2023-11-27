60b284ffca66af9b4519e734f130debefbc27d44
maha404 <117388545+maha404@users.noreply.github.com>
2023-11-27 10:13:50 +0100

    Satte upp en schema för alla poster.

---
+++ server/models/PostModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String, 
+        required: true
+    }, 
+    content: {
+        type: String, 
+        required: true
+    }, 
+    author: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref:"users", 
+        required:true
+    }, 
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "comments"
+    }]
+}, { timestamps: true })
+
+module.exports = mongoose.model('Post', postSchema)
