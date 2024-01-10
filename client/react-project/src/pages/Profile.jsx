4875b549f0187fbec7eb05e3da67a541bcb559e1
maha404 <117388545+maha404@users.noreply.github.com>
2024-01-10 20:20:58 +0100

    Kodade vyn för profilen.

---
+++ client/react-project/src/pages/Profile.jsx
@@ -0,0 +1,48 @@
+// Kod skriven av Maria Halvarsson - Projekt i kursen DT162G
+
+// Bootstrap components 
+import PostForm from '../components/PostForm.jsx';
+import UserPosts from '../components/UserPosts';
+
+// Other tools
+import { useEffect, useState } from 'react';
+
+const Profile = () => {
+
+    const [blogPost, setBlogPost] = useState(null)
+
+    const fetchPosts = async () => {
+        const response = await fetch('http://localhost:3000/blog/user', {
+            method: 'GET', 
+            credentials: 'include'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            setBlogPost(json)
+        }
+    }
+
+    useEffect(() => {
+
+        fetchPosts();
+
+    }, []);
+
+
+    return (
+        <div className='mt-5 mb-5'>
+            <h2 style={{color: 'white', marginLeft:'3rem'}}>Skriv ett inlägg</h2>
+            <div className='bg-light p-2'>
+                <PostForm fetchFunction={fetchPosts} />
+            </div>
+            <div className='posts-container'>
+                {blogPost && blogPost.map((blogPost) => (
+                        <UserPosts key={blogPost._id} blogPost={blogPost} fetchFunction={fetchPosts}/>
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
