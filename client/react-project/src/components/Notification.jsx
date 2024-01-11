c03c41d9aea619771d3d1d661f586ab5f88df385
maha404 <117388545+maha404@users.noreply.github.com>
2024-01-11 15:19:34 +0100

    Lade till att det går att logga ut från notisen.

--- client/react-project/src/components/Notification.jsx
+++ client/react-project/src/components/Notification.jsx
@@ -5,25 +5,45 @@ import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 // Other tools
 import React, { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
+import Auth from '../IsAuth';
 
 const Notification = () => {
 
     const [showReminder, setShowReminder] = useState(false);
+    const navigate = useNavigate();
+    const isAuthenticated = Auth();
 
     useEffect(() => {
         const timeout = setTimeout(() => {
           setShowReminder(true);
+          console.log('timer klar');
         }, 50 * 60 * 1000); 
     
         return () => clearTimeout(timeout);
       }, []);
 
+      const handleClick = async () => {
+        const response = await fetch('http://localhost:3000/blog/logout', {
+          method: 'POST', 
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        
+        if(response.ok) {
+          navigate("/logga_in"); 
+        }
+        
+      }
+
     return (
         <>
-        {showReminder && (
+        {showReminder && isAuthenticated && (
             <Alert variant='info' className='mt-4'>
-            <p>Din session håller på att ta slut! Vänligen logga ut och in igen!</p>
-            <Button>Stäng</Button>
+              <p>Din session håller på att ta slut! Vänligen logga ut och in igen!</p>
+              <Button onClick={handleClick}>Logga ut</Button>
             </Alert>
         )}
         </>
